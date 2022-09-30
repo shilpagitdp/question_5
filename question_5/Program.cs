c8c0d4259c90c2c4d4e7c361538336406800334a
Shilpa.Devanga <Shilpa.Devanga@INLPF2KZ387>
2022-09-30 20:08:21 +0530

    Add project files.

---
+++ question_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return Name + " - " + Age;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Queue<Person> persons = new Queue<Person>();
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Enter name of employee " + (i + 1) + " :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter age of employee " + (i + 1) + " :");
+            int age = int.Parse(Console.ReadLine());
+            persons.Enqueue(new Person()
+            {
+                Name = name,
+                Age = age
+            });
+        }
+        Console.WriteLine("\nPrinting all employee details:");
+        Console.WriteLine("Name - Age");
+        foreach (Person p in persons)
+        {
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
